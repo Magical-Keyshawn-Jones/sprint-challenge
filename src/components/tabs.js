96bf364797e10a37185676b7bbd8b0423a898ae7
Keyshawn Jones <keyshawnjones29@gmail.com>
2022-03-11 16:03:00 -0600

    finished all of tabs

--- src/components/tabs.js
+++ src/components/tabs.js
@@ -13,6 +13,8 @@
   // </div>
   //
 
+import axios from "axios";
+
 const Tabs = (topics) => {
   // Making DOM Variable 
   const topicHolder = document.createElement('div');
@@ -31,17 +33,29 @@ const Tabs = (topics) => {
 
     // Giving topicHolder kids
     topicHolder.appendChild(tabThing);
-  };
+  }
+
+  // Sending Argument through component
+  topics.forEach(string => topicMaker(string))
+
+  // returning component
+  return topicHolder
 }
 
-const tabsAppender = (selector) => {
-  // TASK 4
+ // TASK 4
   // ---------------------
   // Implement this function which takes a css selector as its only argument.
   // It should obtain topics from this endpoint: `http://localhost:5001/api/topics` (test it with a console.log!).
   // Find the array of topics inside the response, and create the tabs using the Tabs component.
   // Append the tabs to the element in the DOM that matches the selector passed to the function.
   //
+
+const tabsAppender = (selector) => {
+  // Grabbing info and storing inside a variable
+
+  axios.get('http://localhost:5001/api/topics')
+  .then(object => document.querySelector(selector).appendChild(Tabs(object.data.topics)))
+  .catch(err => console.log('error!@', err))
 }
 
 export { Tabs, tabsAppender }
